a72caa2a936234d2f095d625161e862280769be9
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2023-09-27 13:52:18 +0600

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,42 @@ import You from "./pages/you/You";
 import Guidance from "./pages/guidance/Guidance";
 import AddFriend from "./pages/compatibility/addFriend/AddFriend";
 import ResultsCompatibilty from "./pages/compatibility/resultsCompatibilty/ResultsCompatibility";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DetailCharacteristic from "./pages/compatibility/detailCharacteristisc/DetailCharacteristic";
+import axios from "axios";
 
 function App() {
   const location = useLocation();
   const footerRoutes = ["/", "/compatibility", "/horoscope", "/you", "/guidance"];
   // Check if the current pathname is in the navbarRoutes array
   const shouldDisplayNavbar = footerRoutes.includes(location.pathname);
+
+  const [user, setUser] = useState(null);
+
+  const data = {
+    email: "nurikgentle@gmail.com",
+    password: "ND#3XAb",
+  };
+
+  const handleSubmit = () => {
+    // Send a POST request using Axios
+    axios
+      .post("https://api.astropulse.app/api/auth", data, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((response) => {
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, []);
 
   const routes = [
     {
